41115384081df677dfc2a8c8366c8c72b38af30f
Francy L. Schenkel <francylauana11@gmail.com>
2026-03-16 15:58:43 -0300

    Add IMC calculation and ngModel bindings
    Replace raw input IDs with Angular two-way bindings and wire a click handler in the template. Add peso, altura, imc and resultado properties and implement calcularIMC() in the component to compute BMI, validate inputs and set a textual classification (underweight, normal, overweight, obesity). Display the computed IMC and classification conditionally in the view.

--- calculo-imc/src/app/app.ts
+++ calculo-imc/src/app/app.ts
@@ -11,4 +11,22 @@ import { CommonModule } from '@angular/common';
 })
 export class App {
   protected readonly title = signal('calculo-imc');
+  peso: number = 0;
+  altura: number = 0;
+  imc: number = 0;
+  resultado: String = '';
+  calcularIMC() {
+    if(this.peso>0 && this.altura>0){
+    this.imc = this.peso / (this.altura * this.altura);
+    if (this.imc < 18.5) {
+      this.resultado = "Abaixo do peso normal.";
+    } else if (this.imc < 24.9) {
+      this.resultado = "Peso normal.";
+    } else if (this.imc < 29.9) {
+      this.resultado = "Excesso de peso.";
+    } else {
+      this.resultado = "Obesidade.";
+    }
+  }
+  }
 }
